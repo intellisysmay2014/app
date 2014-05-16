f5a5a54fa3fadc86e8b141a4f1c135af07e2d5df
juanmcastillo <jcastillo@intellisys.com.do>
2014-05-16 16:53:29 -0400

    Pushing new changes

---
+++ source/app.specs/ReadProductDataSpec.cs
@@ -0,0 +1,49 @@
+﻿ using System.Collections.Generic;
+ using System.Data;
+ using app.web.application.store_browsing;
+ using Machine.Specifications;
+ using developwithpassion.specifications.rhinomocks;
+ using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{  
+    [Subject(typeof(ReadProductData))]  
+    public class ReadProductDataSpec
+    {
+        public abstract class concern : Observes<IQueryForData, ReadProductData>
+        {
+        
+        }
+
+
+        public class when_reading_product_data_from_data_source : concern
+        {
+            private Establish e = () =>
+            {
+                connection = depends.on<IDbConnection>();
+                
+                test_result = new List<ProductLineItem>
+                {
+                    new ProductLineItem
+                    {
+                        name = "product"
+                    }
+                };
+            };
+
+            private Because b = () => 
+                result = sut.find_all_data_where<ProductLineItem>(x => x.name=="product");
+        
+
+            private It should_fetch_all_the_product_related_data_where_name_its_product = () =>
+            {
+                result.ShouldBeTheSameAs(test_result);
+            };
+
+            private static ReadProductData data_reader;
+            private static IEnumerable<ProductLineItem> result;
+            private static IDbConnection connection;
+            private static IEnumerable<ProductLineItem> test_result;
+        }
+    }
+}
---
+++ source/app/web/application/store_browsing/IQueryForData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace app.web.application.store_browsing
+{
+    public interface IQueryForData
+    {
+        IEnumerable<TEntity> find_all_data_where<TEntity>(Func<TEntity, bool> where);
+
+    }
+}
